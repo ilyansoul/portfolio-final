93e7f9c9a031047dd526b0902aaad0c6f9b3d042
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-05-12 16:05:08 +0200

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu-links");
+const icon = document.querySelector(".hamburger-icon");
+
+
+function toggleMenu() {
+ 
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+
+}
+
